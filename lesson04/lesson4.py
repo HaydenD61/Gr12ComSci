4ef06121a0e0b24e09b4016c54915f3dc887f438
HaydenD61 <haydendonahoe@icloud.com>
2025-02-19 14:33:21 -0500

    Lesson4 Solved

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,18 @@
 # Lesson 4
+import math
+
+fences = input("Enter amount of fence planks for section 1 here(FFF):" )
+fences2 = input("Enter amount of fence planks for section 2 here(FFF):" )
+fences3 = input("Enter amount of fence planks for section 3 here(FFF):" )
+
+fences = len(fences)
+fences2 = len(fences2)
+fences3 = len(fences3)
+total = fences + fences2 + fences3
+
+paint = (math.ceil(total / 12))
+paint2 = (math.ceil(total % 12))
+
+cost = paint * 14.95
+
+print(f"You will need {total} buckets of paint, which will cost you ${cost}. You will have {paint2} buckets of paint left over.")
