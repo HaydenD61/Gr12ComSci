9dc540a9c64590c38780adc4798002f11a63f839
HaydenD61 <haydendonahoe@icloud.com>
2025-02-20 14:36:15 -0500

    solved better

--- tutorials/CCC_tutorial.py
+++ tutorials/CCC_tutorial.py
@@ -3,10 +3,7 @@
 userInput = [int(x) for x in input().split()] #input is a string, so we need to convert it to a list of integers
 w1, h1, w2, h2, = userInput
 
-if h1 > h2:
-    ht = h1
-else:
-    ht = h2
+ht = max(h1, h2)
 
 wt = w1 + w2
 
