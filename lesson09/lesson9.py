a572b9fc81c8d3dd55cc38e6324c8a381c1b6642
HaydenD61 <haydendonahoe@icloud.com>
2025-02-21 09:33:24 -0500

    lesson9 solved

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,23 @@
 # Lesson 9
+import random
+
+print ("Thank you for playing Rock, Paper, Scissors!")
+print ("Please choose one of the following:")
+print ("1. Rock")
+print ("2. Paper")
+print ("3. Scissors")
+print ("4. Quit")
+userInput = int(input("Enter your choice (1,2,3,4): "))
+
+choices = [1, 2, 3]
+compChoice = int(random.choice(choices))
+
+if userInput == compChoice:
+    print ("It's a tie!")
+elif userInput ==1 and compChoice == 2 | userInput == 2 and compChoice == 3 | userInput == 3 and compChoice == 1:
+    print ("Computer wins!")
+else:
+    print ("You win!")
+
+print ("Thank you for playing Rock, Paper, Scissors!")
+
