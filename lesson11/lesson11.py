f5de2de5dcc0d2b4fae541515bbd3a7caa547648
HaydenD61 <haydendonahoe@icloud.com>
2025-02-24 13:48:55 -0500

    lesson11 solved

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,15 @@
 # Lesson 11
+
+input = input("Enter x and y coordinates: ")
+input = input.split()
+x = int(input[0])
+y = int(input[1])
+
+if y > 0 and x > 0:
+    print(f"Point ({x}, {y}) is in Quadrant I.")
+elif y > 0 and x < 0:
+    print(f"Point ({x}, {y}) is in Quadrant II.")
+elif y < 0 and x < 0:
+    print(f"Point ({x}, {y}) is in Quadrant III.")
+else:
+    print(f"Point ({x} {y}) is in Quadrant IV.")
