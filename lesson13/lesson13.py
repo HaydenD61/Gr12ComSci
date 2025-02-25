2283b6cbc1fecdfd1cc966c9b0fea5a643222f15
HaydenD61 <haydendonahoe@icloud.com>
2025-02-25 08:32:12 -0500

    lesson13 complete

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,11 @@
 # Lesson 13
+
+inputDate = input("Enter a date (MM DD): ")
+
+
+if inputDate[0] == "02" and inputDate[1] == "18":
+    print("Special")
+elif inputDate < "02 18":
+    print("Before")
+else:
+    print("After")
