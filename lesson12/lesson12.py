a71a2fb89ecd645228804aaa8be4db0a31ebe6d5
HaydenD61 <haydendonahoe@icloud.com>
2025-02-24 22:46:06 -0500

    lesson12 solved

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,19 @@
 # Lesson 12
+
+angle1 = int(input("Enter the first angle: "))
+angle2 = int(input("Enter the second angle: "))
+angle3 = int(input("Enter the third angle: "))
+sum = sum([angle1, angle2, angle3])
+
+
+if sum != 180:
+    print("The angles do not form a triangle.")
+else:
+    if angle1 == angle2 == angle3:
+        print("The triangle is equilateral.")
+    elif angle1 == angle2 or angle2 == angle3 or angle1 == angle3:
+        print("The triangle is isosceles.")
+    else:
+        print("The triangle is scalene.")
+
+
