da8eb3e1f74e07690e5c1a7f7ea866ced0a65411
HaydenD61 <haydendonahoe@icloud.com>
2025-02-25 14:37:22 -0500

    lesson23 solved

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,13 @@
 # Lesson 23
+
+numbers = set()
+
+while True:
+    num = (input("Enter a number/done when done: "))
+    if num == "done":
+        break
+    else:
+        numbers.add(int(num))
+
+average = sum(numbers) // len(numbers)
+print(f"The average of the numbers is {average}")
