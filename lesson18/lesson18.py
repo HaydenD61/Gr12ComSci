f10254afb03aecfcd79e19f0d4f525ab51e2e458
HaydenD61 <haydendonahoe@icloud.com>
2025-02-25 08:54:45 -0500

    lesson18 complete

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,10 @@
 # Lesson 18
+
+num = int(input("Enter a number: "))
+factors = set()
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        factors.add(i)
+        
+print(f"{factors} are the factors of {num}")
