1672439f01e39db0c56bfb9556b036395038f409
HaydenD61 <haydendonahoe@icloud.com>
2025-02-25 09:08:39 -0500

    lesson19 solved

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,13 @@
 # Lesson 19
+
+num = int(input("Enter a number: "))
+i = 1
+
+for i in range(1, num + 1):
+    if num % i == 0 and i != 1 and i != num:
+        print(f"{num} is not a prime number")
+        break
+    elif num % i > 0 or i == num:
+        print(f"{num} is a prime number")
+        break
+
