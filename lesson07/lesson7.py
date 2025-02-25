76ef1223a86c02fc487c4bd8baf59968a19e8a3c
HaydenD61 <haydendonahoe@icloud.com>
2025-02-25 18:08:00 -0500

    lesson7 solved

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,13 @@
 # Lesson 7
+from random import randrange
+
+
+dc = int(input("Enter the DC level: "))
+dice = randrange(1, 20)
+
+if dice >= dc:
+    print(f"You rolled a {dice} and passed the DC")
+else:
+    print(f"You rolled a {dice} and failed the DC")
+
+
