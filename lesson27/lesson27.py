db6ed97a2064f2166fc96453e74c38d6ccdd1c38
HaydenD61 <haydendonahoe@icloud.com>
2025-03-04 09:31:40 -0500

    lesson27 complete

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,11 @@
-# Lesson 27
+# StringCleaner
+
+text = input("Enter an uncleaned string: ")
+alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+cleanText = []
+for char in text:
+    if char in alphabet:
+        cleanText.append(char)
+
+word = "".join(cleanText)
+print(word)
