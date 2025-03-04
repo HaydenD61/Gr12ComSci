88b3e81867463bada2bfb4b789bf6a225c359631
HaydenD61 <haydendonahoe@icloud.com>
2025-03-04 09:36:55 -0500

    lesson28 complete

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,13 @@
 # Lesson 28
+
+word = input("Enter word: ")
+
+if word[0] != word[-1]:
+    print("Not a palindrome!")
+else:
+    word1 = word[:: -1]
+    if word == word1:
+        print("Palindrome!!")
+    else:
+        print("NOT A PALINDROME!!")
+
