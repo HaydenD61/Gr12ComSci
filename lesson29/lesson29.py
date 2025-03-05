78276cec33bbe829b50a6a91a5e146bba10be001
HaydenD61 <haydendonahoe@icloud.com>
2025-03-05 12:53:41 -0500

    lesson29 complete

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,29 @@
 # Lesson 29
+
+def consonantCheck(text, vowel=False):
+    ''' determines number of consonants in a word
+
+    '''
+    
+    counter = 0
+
+    for character in text:
+        character = character.lower()
+        if character.isalpha() and character not in {'a', 'e', 'i', 'o', 'u'}:
+            counter += 1
+    
+    if not vowel:
+        return counter
+    else:
+        # return len(text) - counter
+        counter = 0
+
+        for character in text:
+            character = character.lower()
+            if character.isalpha() and character in {'a', 'e', 'i', 'o', 'u'}:
+                counter += 1
+        return counter
+
+
+print(f'the count is: {consonantCheck("Hello World!")}')
+print(f'the count is: {consonantCheck("Hello World!", True)}')
