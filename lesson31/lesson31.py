204171065251931c9fffc04c1ca09fe273fc39b1
HaydenD61 <haydendonahoe@icloud.com>
2025-03-06 13:47:17 -0500

    lesson31 complete

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,23 @@
 # Lesson 31
+
+def anaCheck(word1, word2):
+
+    aList = list(word1)
+    bList = list(word2)
+
+    if word1 == word2:
+        word = "ANAGRAM!"
+    elif len(aList) == len(bList):
+        for i in aList:
+            if i in bList:
+                bList.remove(i)
+        if len(bList) == 0:
+            word = "ANAGRAM!"
+    else:
+        word = "NOT AN ANAGRAM!!"  
+
+    return word
+
+word1 = input("Enter word 1: ")
+word2 = input("Enter word 2: ")
+print(anaCheck(word1, word2))
