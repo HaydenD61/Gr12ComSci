871f9fc391550df1eb01141c623e8b876668205a
HaydenD61 <haydendonahoe@icloud.com>
2025-03-19 13:07:49 -0400

    solved

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,20 @@
 # Lesson 32
+
+def letter(word1, word2):
+
+    aList = list(word1)
+    bList = list(word2)
+    shared = []
+
+    if word1 == word2:
+        shared = list(word1)
+    else:
+        for i in aList:
+            if i in bList:
+                shared.append(i)
+    
+    return sorted(shared)
+
+word1 = input("Enter first word: ")
+word2 = input("Enter second word: ")
+print(letter(word1, word2))
