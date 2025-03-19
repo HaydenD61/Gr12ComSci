60697e42a053b3b4f2a6e87c1e6f8768d42c20a8
HaydenD61 <haydendonahoe@icloud.com>
2025-03-19 13:23:08 -0400

    solved

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,27 @@
 # Lesson 33
+
+def mean(nums):
+    
+    avrg = sum(nums) // len(nums)
+
+    return avrg
+
+def median(nums):
+
+    ordered = sorted(nums)
+    mid = (len(ordered) // 2) - 1
+
+    return (ordered[mid])
+
+
+nums = []
+while True:
+    inpt = input("Enter number: ")
+    if inpt.lower() == 'x':
+        break
+    else:
+        nums.append(int(inpt))
+
+print(f"Mean: {mean(nums)}")
+print(f"Median: {median(nums)}")
+
