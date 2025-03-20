242dfb8613400af3810d639d574ac05934828136
HaydenD61 <haydendonahoe@icloud.com>
2025-03-20 08:37:03 -0400

    solved

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,19 @@
 # Lesson 37
+
+def compress(word):
+    compressed = ''
+    count = 1
+
+    for i in range(len(word)-1):
+        if word[i] == word[i+1]:
+            count += 1
+        else: 
+            compressed += word[i] + str(count)
+            count = 1
+        
+    compressed += word[-1] + str(count)
+    return compressed
+
+print(compress(input('Enter Word Here: ')))
+        
+        
