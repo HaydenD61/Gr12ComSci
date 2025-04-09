dba629bd2a042f6ed8caf104a8183a87123e04d2
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:00:25 -0400

    solved

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,15 @@
 # Lesson 21
+
+num = int(input("Enter a number: "))
+factors = set()
+
+for i in range(1, num + 1):
+    factors1 = set()
+    for j in range(1, i):
+        if num % j == 0:
+            factors.add(j)
+    factors.add(len(factors1))
+
+largest = (factors)
+print(f"The number with the most factors is {largest}")
+
