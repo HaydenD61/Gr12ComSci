9174e349939e27673f60d4155f438d56f998aa7b
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:06:13 -0400

    solved

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,13 @@
 # Lesson 10
+
+num = [input("Enter last 4 digits of phone number: ")]
+
+i = 0
+if num[i] == 8 or num[i] == 9:
+    i += 1
+    if num [i] == num [i + 1]:
+        i += 2
+        if num [i] == 8 or num [i] == 9:
+            print("Number is a scammer.")
+else:
+    print("Number is not a scammer.")
