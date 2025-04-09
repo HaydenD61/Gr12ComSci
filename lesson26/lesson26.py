3182dec7c9b8b3344ff2a60353cc7f062983a2e1
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:14:34 -0400

    solved

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,24 @@
 # Lesson 26
+
+def factorCount(number):
+    ''' determines number of factors a number has
+
+    Args:
+        number: an integer needed to determine the number of its factors
+    
+    Returns:
+        an integer, which is the total amount of factors from 1 to N 
+    '''
+    counter = 0
+    for divider in range(1, number+1):
+        if number % divider == 0:
+            counter +=1
+    
+    return counter
+
+upperLimit = int(input("Enter N: "))
+
+for num in range(1, upperLimit+1):
+    factorSize = factorCount(num)
+
+    print(f"{num} has {factorSize} factors.")
