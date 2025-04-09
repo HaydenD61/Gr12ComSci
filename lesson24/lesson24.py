df352c88928017e6a9d0fe35ed605c6dc37d45dc
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:05:42 -0400

    sovled

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,15 @@
 # Lesson 24
+
+names = []
+while True:
+    name = input("Enter a name (or 'done' to finish): ")
+    if name.lower() == 'done':
+        break
+    names.append(name)
+
+large = names[0]
+for i in names:
+    if i > large:
+        large = i
+
+print("The largest name is:", large)
