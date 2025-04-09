c7a0d99e1216fb63761e09fe250d381457c180d7
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 13:59:55 -0400

    solved

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,18 @@
 # Lesson 20
+
+
+for i in range(1, 10000):
+    factors = set()
+    for n in range(1, i + 1):
+        while n < i:
+            if i % n == 0:
+                factors.add(i)
+                n += 1
+            
+        if sum(factors) == i:
+            print(f"{i} is a perfect number")
+
+
+
+
+        
