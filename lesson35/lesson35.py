2cce48e582c70a6cce008f9cfa9875d6f71f1d4e
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:29:11 -0400

    solved

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,14 @@
 # Lesson 35
+
+def removeDuplicates(aList):
+    """Remove duplicates from a list."""
+    newList = []
+    for item in aList:
+        if item not in newList:
+            newList.append(item)
+    return newList
+
+aList = [1, 1, 1, 2, 2, 3, 4, 5, 3, 3, 4, 5, 6, 7, 8, 9]
+print("Original list:", aList)
+bList = removeDuplicates(aList)
+print("List with duplicates removed:", bList)
