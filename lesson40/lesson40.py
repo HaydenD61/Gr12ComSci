49d43ce80f3c8079793915e07d345b1b86e4fb9d
HaydenD61 <haydendonahoe@icloud.com>
2025-04-09 14:17:25 -0400

    solved

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,14 @@
 # Lesson 40
+
+def prime(num):
+    
+    primes = []
+    for i in range (1, num):
+        if i % 2 > 0:
+            if i % 3 > 0:
+                primes.append(i)
+    return primes
+
+num = int(input('Enter number here: '))
+print(prime(num))
+        
